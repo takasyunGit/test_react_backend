1f619736ad1370d1a89afb3cd241d15b3d9a98c3
shun0610 <graphic228@yahoo.co.jp>
2024-03-15 17:29:10 +0900

    [add]handle_errorをApplicationControllerに追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,19 +9,20 @@ class ApplicationController < ActionController::Base
   rescue_from StandardError, with: :render_500
   rescue_from ActiveRecord::RecordNotFound, ActionController::RoutingError, with: :render_404
 
-  def render_404(e = nil)
+  def handle_error(e = nil)
     if e
       logger.error e
       logger.error e.backtrace.join("\n")
     end
+  end
+
+  def render_404(e = nil)
+    handle_error(e)
     render json: { status: 404, messages: "Page not found" }
   end
 
   def render_500(e = nil)
-    if e
-      logger.error e
-      logger.error e.backtrace.join("\n")
-    end
+    handle_error(e)
     render json: { status: 500, messages: "An unexpected error has occurred" }
   end
 end
