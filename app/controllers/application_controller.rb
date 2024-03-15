07c54d89bd3a3b73508db38265fbaa0fa921f027
shun0610 <graphic228@yahoo.co.jp>
2024-03-15 15:40:11 +0900

    [add]render_500追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,13 @@ class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
   helper_method :current_user, :user_signed_in?
 
-  def set_csrf_token
-    cookies['CSRF-TOKEN'] = {
-      domain: 'localhost', # フロント側ドメイン
-      value: form_authenticity_token
-    }
+  rescue_from StandardError, with: :render_500
+
+  def render_500(e = nil)
+    if e
+      logger.error e
+      logger.error e.backtrace.join("\n")
+    end
+    render json: { status: 500, messages: "An unexpected error has occurred" }
   end
 end
