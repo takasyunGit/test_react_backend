a237f8e8fe26d917b986a7d7a1f853858c70781c
shun0610 <graphic228@yahoo.co.jp>
2024-03-25 15:39:47 +0900

    [add]add_ShowVendorOffer_page_to_User

---
+++ app/controllers/api/v1/user/vendor_offers_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::User::VendorOffersController < ApplicationController
+  before_action :authenticate_api_v1_user!
+
+  def show
+    from_table = VendorOffer.where(id: params[:id])
+    @object = VendorOffer
+      .select("vendor_offers.*, users.name")
+      .from(from_table, :vendor_offers)
+      .joins(user_offer: :user)
+    if @object
+      render json: { data: @object.first }
+    else
+      render_404
+    end
+  end
+end
