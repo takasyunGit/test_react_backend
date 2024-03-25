3b6df1d4ef5e85b01dc983f8bf0735943cc20704
shun0610 <graphic228@yahoo.co.jp>
2024-03-25 17:45:56 +0900

    [add]ShowUserOfferのページに提案されているVendorOfferを表示

--- app/controllers/api/v1/user/vendor_offers_controller.rb
+++ app/controllers/api/v1/user/vendor_offers_controller.rb
@@ -1,6 +1,20 @@
 class Api::V1::User::VendorOffersController < ApplicationController
   before_action :authenticate_api_v1_user!
 
+  def index
+    from_table = VendorOffer.where(user_offer_id: params[:user_offer_id])
+    @object = VendorOffer
+      .select("vendor_offers.*, users.name")
+      .from(from_table, :vendor_offers)
+      .joins(user_offer: :user)
+      .where("user_offers.user_id": current_user.id)
+    if @object
+      render json: { data: @object }
+    else
+      render_404
+    end
+  end
+
   def show
     from_table = VendorOffer.where(id: params[:id])
     @object = VendorOffer
