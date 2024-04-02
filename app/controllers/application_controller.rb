c26b63eb08d26cb8221d5f18b1f07aec1da628b1
shun0610 <graphic228@yahoo.co.jp>
2024-04-02 17:34:49 +0900

    [back][add]401エラーをapplicationコントローラーに追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,15 @@ class ApplicationController < ActionController::Base
   include ActionController::Cookies
   include ActionController::RequestForgeryProtection
 
+  UnauthorizedError = Class.new(StandardError)
+
   skip_before_action :verify_authenticity_token
   helper_method :current_user, :user_signed_in?
 
   rescue_from StandardError, with: :render_500
   rescue_from ActiveRecord::RecordNotFound, ActionController::RoutingError, with: :render_404
   rescue_from ActiveRecord::RecordInvalid, with: :render_record_invalid
+  rescue_from UnauthorizedError, with: :render_401
 
   def handle_error(e = nil)
     if e
@@ -22,6 +25,11 @@ class ApplicationController < ActionController::Base
     render json: { errors: { fullMessages: e.record.errors.full_messages } }, status: 422
   end
 
+  def render_401(e = nil)
+    handle_error(e)
+    render json: { messages: "Unauthorized" }, status: 401
+  end
+
   def render_404(e = nil)
     handle_error(e)
     render json: { messages: "Page not found" }, status: 404
@@ -33,10 +41,9 @@ class ApplicationController < ActionController::Base
   end
 
   def common_authentication!
-    if params[:sign_in_type] == "User"
-      current_user = current_api_v1_user
-    else
-      current_user = current_api_v1_vendor_user
+    current_user = current_api_v1_user || current_api_v1_vendor_user
+    unless current_user
+      raise UnauthorizedError
     end
   end
 end
