fab1024b5347b759f6eea746d0581093f9952c22
shun0610 <graphic228@yahoo.co.jp>
2024-04-05 17:12:46 +0900

    [back][upadte]carrierwaveに関するparamsをストロングパラメータに追加

--- app/controllers/api/v1/user/registrations_controller.rb
+++ app/controllers/api/v1/user/registrations_controller.rb
@@ -1,7 +1,11 @@
 class Api::V1::User::RegistrationsController < DeviseTokenAuth::RegistrationsController
   private
 
-    def sign_up_params
-      params.permit(:email, :password, :password_confirmation, :name)
-    end
+  def sign_up_params
+    params.permit(:email, :password, :password_confirmation, :name)
+  end
+
+  def account_update_params
+    params.permit(:avatar)
+  end
 end
--- app/controllers/api/v1/vendor_user/registrations_controller.rb
+++ app/controllers/api/v1/vendor_user/registrations_controller.rb
@@ -1,7 +1,11 @@
 class Api::V1::VendorUser::RegistrationsController < DeviseTokenAuth::RegistrationsController
   private
 
-    def sign_up_params
-      params.permit(:email, :password, :password_confirmation, :name, :vendor_id)
-    end
+  def sign_up_params
+    params.permit(:email, :password, :password_confirmation, :name, :vendor_id)
+  end
+
+  def account_update_params
+    params.permit(:avatar)
+  end
 end
