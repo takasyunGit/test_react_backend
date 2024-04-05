1bfbaacfbcb1b88a5cd9947ad302f1a6a8117ce0
shun0610 <graphic228@yahoo.co.jp>
2024-04-05 20:27:02 +0900

    [back][upate]vendorOfferChatsコントローラーのindexでavatarの値を返すように修正

--- app/controllers/api/v1/vendor_offer_chats_controller.rb
+++ app/controllers/api/v1/vendor_offer_chats_controller.rb
@@ -1,10 +1,17 @@
 class Api::V1::VendorOfferChatsController < ApplicationController
   NUMBER_OF_PER_PAGE = 10
+  require 'uri'
 
   before_action :common_authentication!
 
   def index
-    @objects = VendorOfferChat.select("vendor_offer_chats.*, COALESCE(users.name, vendor_users.name, '不明なユーザー') as name")
+    @objects = VendorOfferChat.select(
+        <<~SQL.gsub(/\n/," ")
+          vendor_offer_chats.*,
+          COALESCE(users.name, vendor_users.name, '不明なユーザー') as name,
+          COALESCE(users.avatar, vendor_users.avatar) as avatar
+        SQL
+      )
       .where(vendor_offer_id: params[:vendor_offer_id])
       .joins(
         <<~SQL
@@ -13,6 +20,20 @@ class Api::V1::VendorOfferChatsController < ApplicationController
         SQL
       )
     @objects = @objects.paginate_order(params[:key_id], "desc", NUMBER_OF_PER_PAGE, "created_at")
+    # avatarのurlをfullpathで返却するために加工
+    # ここのpathは本番環境で変わるので要修正
+    domain = Rails.env.production? ? "http://localhost:3010" : "http://localhost:3010"
+    @objects[:records] = @objects[:records].map do |obj|
+      if obj.avatar
+        if obj.user_id
+          obj.avatar = { url: "#{domain}/uploads/user/avatar/#{obj.user_id}/#{URI.encode_www_form_component(obj.avatar)}" }
+        end
+        if obj.vendor_user_id
+          obj.avatar = { url: "#{domain}/uploads/vendor_user/avatar/#{obj.vendor_user_id}/#{URI.encode_www_form_component(obj.avatar)}" }
+        end
+      end
+      obj
+    end
     render json: { data: @objects }
   end
 
