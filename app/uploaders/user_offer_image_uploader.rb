38350832ad74c2df3c634b5b02c2a1e6ad97652a
shun0610 <graphic228@yahoo.co.jp>
2024-04-18 17:43:16 +0900

    [back][update]userOfferの画像にthumbのイメージを作成

--- app/uploaders/user_offer_image_uploader.rb
+++ app/uploaders/user_offer_image_uploader.rb
@@ -22,16 +22,16 @@ class UserOfferImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process resize_to_fill: [300, 170, "Center"]
+  # process resize_to_fill: [300, 170, "Center"]
 
   # def scale(width, height)
   #   # do something
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
+  version :thumb do
+    process resize_to_fill: [300, 200, "Center"]
+  end
 
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
