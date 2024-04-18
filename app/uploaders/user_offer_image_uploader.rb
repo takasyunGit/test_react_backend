7b8d718b18ff5455567dade7432f23a2e6172c73
shun0610 <graphic228@yahoo.co.jp>
2024-04-18 17:55:08 +0900

    [back][update]アップロードできる画像サイズを制限

--- app/uploaders/user_offer_image_uploader.rb
+++ app/uploaders/user_offer_image_uploader.rb
@@ -7,6 +7,10 @@ class UserOfferImageUploader < CarrierWave::Uploader::Base
   storage :file
   # storage :fog
 
+  def size_range
+    1..5.megabytes
+  end
+
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
