943c69c3e4b0cb62a61a0109bad59737afda7abe
shun0610 <graphic228@yahoo.co.jp>
2024-04-19 18:46:45 +0900

    [back][update]validation削除 エラーメッセージの際に複数出るのが気になったので削除。ユーザー側の入力に関係するvalidationではないので削除した

--- app/models/vendor_offer_image.rb
+++ app/models/vendor_offer_image.rb
@@ -1,8 +1,5 @@
 class VendorOfferImage < ApplicationRecord
   mount_uploader :content, VendorOfferImageUploader
 
-  validates :vendor_offer_id, presence: true
-  validates :content, presence: true
-
   belongs_to :vendor_offer
 end
