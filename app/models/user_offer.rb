4adf4968a5c67fc224128261d41c9c101934f3a5
shu- <test@test.test>
2024-05-08 19:36:55 +0900

    [add]UserOfferにstatusの項目追加

--- app/models/user_offer.rb
+++ app/models/user_offer.rb
@@ -10,4 +10,5 @@ class UserOffer < ApplicationRecord
   validates :prefecture, presence: true, numericality: { in: 1..47 }
   validates :address, length: { maximum: MAXIMUM_ADDRESS_LENGTH }
   validates :request_type, presence: true, numericality: { in: 1..3 }
+  validates :status, presence: true, numericality: { in: 1..4 }
 end
