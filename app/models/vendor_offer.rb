a986276d1c823178c0a649e30fe4fb75bcfc7caf
shun0610 <graphic228@yahoo.co.jp>
2024-05-08 13:52:50 +0900

    [update]vendorOfferChat作成時にエラーが発生するバグの修正

--- app/models/vendor_offer.rb
+++ app/models/vendor_offer.rb
@@ -1,8 +1,6 @@
 class VendorOffer < ApplicationRecord
   MAXIMUM_TITLE_LENGTH = 100
 
-  mount_uploader :avatar, AvatarUploader
-
   has_many :vendor_offer_chats, dependent: :destroy
   has_many :vendor_offer_images, dependent: :destroy
 
