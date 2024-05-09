edf76a5b3aeaf24b30b8b86a5cfc362ae9c79bb8
shu- <test@test.test>
2024-05-09 18:04:35 +0900

    [update]VendorHome画面にavatarが表示される用に修正

--- app/controllers/api/v1/vendor_user/user_offers_controller.rb
+++ app/controllers/api/v1/vendor_user/user_offers_controller.rb
@@ -2,13 +2,37 @@ class Api::V1::VendorUser::UserOffersController < ApplicationController
   before_action :authenticate_api_v1_vendor_user!
 
   def index
-    my_proposal_offer = UserOffer.joins(
-      <<~SQL.gsub(/\n/," ")
-        INNER JOIN (SELECT * FROM vendor_offers WHERE vendor_user_id = #{current_api_v1_vendor_user.id}) as my_offers
-        ON my_offers.user_offer_id = user_offers.id
-      SQL
-    ).order(status: :asc).order(deadline: :desc)
+    UserOffer.mount_uploader(:avatar, AvatarUploader)
+
+    my_proposal_offer = UserOffer
+      .select(
+        <<~SQL.gsub(/\n/," ")
+          user_offers.*,
+          users.name as user_name,
+          users.avatar as avatar
+        SQL
+      )
+      .joins(
+        <<~SQL.gsub(/\n/," ")
+          INNER JOIN
+            (SELECT * FROM vendor_offers WHERE vendor_user_id = #{current_api_v1_vendor_user.id}) as my_offers
+          ON
+            my_offers.user_offer_id = user_offers.id
+          INNER JOIN
+            users
+          ON
+            users.id = user_offers.user_id
+        SQL
+      ).order(status: :asc).order(deadline: :desc)
     not_touched_offers = UserOffer
+      .select(
+        <<~SQL.gsub(/\n/," ")
+          user_offers.*,
+          users.name as user_name,
+          users.avatar as avatar
+        SQL
+      )
+      .joins(:user)
       .where(status: USER_OFFER_STATUS_PROPOSAL)
       .where.not(id: my_proposal_offer.ids)
       .order(status: :asc).order(deadline: :desc)
